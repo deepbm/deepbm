11bdd231c084747e6f2e2a89473ae6e8aa66e996
양희연 <deepbm27@gmail.com>
2023-07-25 15:17:58 +0900

    Add: Experience component

---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Experience.css';
+
+export default function Experience() {
+  return (
+    <section className='experience'>
+      <div className='achievement'>
+        <div className='circle'>1+</div>
+        <span>years</span>
+        <span>Experience</span>
+      </div>
+      <div className='achievement'>
+        <div className='circle'>5+</div>
+        <span>completed</span>
+        <span>Projects</span>
+      </div>
+    </section>
+  );
+}
